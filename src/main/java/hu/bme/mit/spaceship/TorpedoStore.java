5d05bc4555ec136414825b543db7f389ea3b12f5
Petra <petrahidegkuti@edu.bme.hu>
2025-10-06 16:13:47 +0200

    changed =- to -=

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -43,7 +43,7 @@ Random generator = new Random();
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount =- numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
